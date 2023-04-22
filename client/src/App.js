2054698616c08ad58dca751b6f7eece52cca68d0
Duncan Applegarth <duncana513@gmail.com>
2023-04-22 10:49:26 -0700

    add html for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appContainer">
+      <h1>Book List Thingy!</h1>
+
+      <div className="newBookContainer">
+        <h2>Add a New Book</h2>
+        <form>
+          <div>
+            <label htmlFor="newBookNameInput">Name</label>
+            <input type="text" id="newBookNameInput" />
+          </div>
+
+          <div>
+            <label htmlFor="newBookAuthor">Author</label>
+            <input type="text" id="newBookAuthor" />
+          </div>
+
+          <div>
+            <label htmlFor="newBookDescription">Description</label>
+            <input type="text" id="newBookDescription" />
+          </div>
+
+          <button type="submit">Add Book</button>
+        </form>
+      </div>
+
+      <div className="bookListContainer">
+        <h2>List of Books</h2>
+
+        <ul>
+          <li>
+            <div class="bookInfo">
+              <p>
+                <b>Name:</b> hello!
+              </p>
+              <p>
+                <b>Author:</b> dookan
+              </p>
+              <p>
+                <b>Description:</b> this is quite the book
+              </p>
+            </div>
+
+            <div class="buttonActions">
+              <button type="button" className="btn">
+                Edit Book Info
+              </button>
+
+              <button type="button" className="btn">
+                Delete Book
+              </button>
+            </div>
+          </li>
+
+          <li>
+            <div class="bookInfo">
+              <p>
+                <b>Name:</b> hello!
+              </p>
+              <p>
+                <b>Author:</b> dookan
+              </p>
+              <p>
+                <b>Description:</b> this is quite the book
+              </p>
+            </div>
+
+            <div class="buttonActions">
+              <button type="button" className="btn">
+                Edit Book Info
+              </button>
+
+              <button type="button" className="btn">
+                Delete Book
+              </button>
+            </div>
+          </li>
+
+          <li>
+            <div class="bookInfo">
+              <p>
+                <b>Name:</b> hello!
+              </p>
+              <p>
+                <b>Author:</b> dookan
+              </p>
+              <p>
+                <b>Description:</b> this is quite the book
+              </p>
+            </div>
+
+            <div class="buttonActions">
+              <button type="button" className="btn">
+                Edit Book Info
+              </button>
+
+              <button type="button" className="btn">
+                Delete Book
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
