45f29d1efd40426d77b23fb650eecc897e5398db
Duncan Applegarth <duncana513@gmail.com>
2023-04-22 11:46:06 -0700

    add state to BookItem

--- client/src/components/BookItem.jsx
+++ client/src/components/BookItem.jsx
@@ -1,21 +1,50 @@
+import { useState } from "react";
+
 export default function BookItem(props) {
+  const [name, setName] = useState(props.name ?? "No name");
+  const [author, setAuthor] = useState(props.author ?? "No author");
+  const [description, setDescription] = useState(
+    props.description ?? "No description"
+  );
+
+  function handleTextChange(e, updateStateFn) {
+    updateStateFn(e.target.value);
+  }
+
   return (
     <li>
       <div class="bookInfo">
-        <p>
-          <b>Name:</b> {props.name ?? "No name"}
-        </p>
-        <p>
-          <b>Author:</b> {props.author ?? "No author"}
-        </p>
-        <p>
-          <b>Description:</b> {props.description ?? "No description"}
-        </p>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            onChange={(e) => handleTextChange(e, setName)}
+            value={name}
+          />
+        </div>
+
+        <div>
+          <label>Author:</label>
+          <input
+            type="text"
+            onChange={(e) => handleTextChange(e, setAuthor)}
+            value={author}
+          />
+        </div>
+
+        <div>
+          <label>Description:</label>
+          <input
+            type="text"
+            onChange={(e) => handleTextChange(e, setDescription)}
+            value={description}
+          />
+        </div>
       </div>
 
       <div class="buttonActions">
         <button type="button" className="btn">
-          Edit Book Info
+          Update Book Info
         </button>
 
         <button type="button" className="btn">
